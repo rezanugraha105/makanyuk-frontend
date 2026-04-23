913547b63152171fada2b4e7b9d4787712cf7181
Reza Nugraha <rezanugraha105@gmail.com>
2026-04-23 14:00:23 +0700

    Update

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
-  BASE_URL: 'https://makanyukapi-h2kg5dk6.b4a.run',
+  BASE_URL: process.env.BASE_URL,
   ACCESS_TOKEN_KEY: 'accessToken',
   REFRESH_TOKEN_KEY: 'refreshToken',
-  MAP_SERVICE_API_KEY: '4P7wZaKkzOjTOU8nnftC',
+  MAP_SERVICE_API_KEY: process.env.MAP_SERVICE_API_KEY,
 };
 
 export default CONFIG;
