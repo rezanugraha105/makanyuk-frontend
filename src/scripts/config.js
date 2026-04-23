067e70860b9f538b58116e6c919ca27824c83b51
Reza Nugraha <rezanugraha105@gmail.com>
2026-04-23 14:14:12 +0700

    Rollback

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
-  BASE_URL: process.env.BASE_URL,
+  BASE_URL: 'https://makanyukapi-h2kg5dk6.b4a.run',
   ACCESS_TOKEN_KEY: 'accessToken',
   REFRESH_TOKEN_KEY: 'refreshToken',
-  MAP_SERVICE_API_KEY: process.env.MAP_SERVICE_API_KEY,
+  MAP_SERVICE_API_KEY: '4P7wZaKkzOjTOU8nnftC',
 };
 
 export default CONFIG;
